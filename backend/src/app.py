23fa0c0126b03c8ca6550ee3a370d4d9d32669b3
Marcos Tovar <87570696+marcos65333@users.noreply.github.com>
2025-04-11 17:04:56 -0500

    Add files via upload

---
+++ backend/src/app.py
@@ -0,0 +1,6 @@
+from src import app
+
+# This is the main entry point for the Flask application.
+# It imports the app instance from the src module and runs it.
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
